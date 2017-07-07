b03a58e6c179655107d7def1d12958eb516f032c
Christy Sparks <csparks91@gmail.com>
2017-07-07 09:20:51 -0500

    Fetch Added

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -5,6 +5,17 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
+const url = 'http://localhost:3000/records';
+fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then(function() {
+    // Your code for handling the data you get from the API
+    let records = data.results;
+    return records;
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+    console.log("Something is wrong.");
+});
 
 
 export default retrieve;
